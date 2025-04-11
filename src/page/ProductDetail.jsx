152f797a6e061f4c808db8eeb68700cc086190fb
roseraph502on <sera6768@naver.com>
2025-04-11 13:30:08 +0900

    제품상세 url

--- src/page/ProductDetail.jsx
+++ src/page/ProductDetail.jsx
@@ -9,7 +9,7 @@ const ProductDetail = () => {
   const [product, setProduct] = useState(null);
   const [selectedSize, setSelectedSize] = useState('');
   const getProductDetail = async () => {
-    let url = `http://localhost:5000/products/${id}`
+    let url = `https://my-json-server.typicode.com/roseraph502on/shoppingMall/products/${id}`
     let response = await fetch(url);
     let data = await response.json();
     console.log("paramd", data)
